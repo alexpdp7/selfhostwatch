946d3c6bf7a435edda39c1c095b7bef7609fd397
alex <alex@pdp7.net>
2024-10-27 13:25:05 +0100

    Normalize and improve fields

--- shw/yuno/__init__.py
+++ shw/yuno/__init__.py
@@ -1,3 +1,4 @@
+from collections import abc
 import dataclasses
 
 import httpx
@@ -7,32 +8,39 @@ def load():
     return httpx.get("https://apps.yunohost.org/default/v3/apps.json").json()
 
 
-def parse(apps_json):
-    return [app_from_json(j) for j in apps_json["apps"].values()]
-
-
 @dataclasses.dataclass
 class App:
     id: str
-    architectures: list[str]
+    architectures: set[str]
     version: str
-    yuno_ldap: bool
+    yuno_ldap: str # true, false, not_relevant
     yuno_multi_instance: bool
-    yuno_sso: bool
+    yuno_sso: str  # true, false, not_relevant
     yuno_high_quality: bool
     yuno_maintained: bool
     yuno_state: str
 
 
 def app_from_json(j):
+    architectures = j["manifest"]["integration"]["architectures"]
+
+    if architectures == "all":
+        architectures = set()
+    else:
+        architectures = set(architectures)
+
     return App(
         id=j["id"],
-        architectures=j["manifest"]["integration"]["architectures"],
+        architectures=architectures,
         version=j["manifest"]["version"],
-        yuno_ldap=j["manifest"]["integration"]["ldap"],
+        yuno_ldap=str(j["manifest"]["integration"]["ldap"]).lower(),
         yuno_multi_instance=j["manifest"]["integration"]["multi_instance"],
-        yuno_sso=j["manifest"]["integration"]["sso"],
+        yuno_sso=str(j["manifest"]["integration"]["sso"]).lower(),
         yuno_high_quality=j["high_quality"],
         yuno_maintained=j["maintained"],
         yuno_state=j["state"],
     )
+
+
+def parse(apps_json) -> abc.Iterator[App]:
+    return [app_from_json(j) for j in apps_json["apps"].values()]
