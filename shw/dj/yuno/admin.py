c5bf4fb5e99389c79e95c22348ab8f06f4436774
alex <alex@pdp7.net>
2024-11-18 21:55:08 +0100

    Tune yuno.AppVersion admin

--- shw/dj/yuno/admin.py
+++ shw/dj/yuno/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 from shw.dj.yuno import models
 
 
-admin.site.register(models.AppVersion)
+class AppVersionAdmin(admin.ModelAdmin):
+    list_display = ["name", "version", "updated"]
+
+
+admin.site.register(models.AppVersion, AppVersionAdmin)
 admin.site.register(models.Architecture)
