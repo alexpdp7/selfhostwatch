f2fc7509272d4208a3981839ae4b20846ca0e530
alex <alex@pdp7.net>
2024-11-18 21:51:18 +0100

    Tweak git.Version admin

--- shw/dj/git/admin.py
+++ shw/dj/git/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 from shw.dj.git import models
 
 
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ["git_app__name", "version", "date"]
+
+
 admin.site.register(models.GitApp)
-admin.site.register(models.Version)
+admin.site.register(models.Version, VersionAdmin)
