6782920a39321aacefef35bc5eef71c967ef1f9c
alex <alex@pdp7.net>
2024-11-20 20:52:13 +0100

    Add some error handling

--- shw/dj/git/loader.py
+++ shw/dj/git/loader.py
@@ -3,12 +3,17 @@ from shw.dj.git import models
 
 
 def load():
+    broken = {}
     for git_app in models.GitApp.objects.all():
-        tags_to_commits = git.get_remote_tags_to_dates(git_app.remote_url)
+        try:
+            tags_to_commits = git.get_remote_tags_to_dates(git_app.remote_url)
 
-        for tag, date in tags_to_commits.items():
-            models.Version.objects.update_or_create(
-                git_app=git_app,
-                version=tag,
-                defaults={"date": date}
-            )
+            for tag, date in tags_to_commits.items():
+                models.Version.objects.update_or_create(
+                    git_app=git_app,
+                    version=tag,
+                    defaults={"date": date}
+                )
+        except Exception as e:
+            broken[git_app.name] = e
+    assert not broken, f"Broken repos {broken}"
