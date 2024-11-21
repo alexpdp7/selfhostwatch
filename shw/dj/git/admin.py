cc75bb97368b3c6c80de627e0d86155bb13db3cd
alex <alex@pdp7.net>
2024-11-21 20:43:22 +0100

    Add search to GitApp admin

--- shw/dj/git/admin.py
+++ shw/dj/git/admin.py
@@ -3,9 +3,13 @@ from django.contrib import admin
 from shw.dj.git import models
 
 
+class GitAppAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+
+
 class VersionAdmin(admin.ModelAdmin):
     list_display = ["git_app__name", "version", "date"]
 
 
-admin.site.register(models.GitApp)
+admin.site.register(models.GitApp, GitAppAdmin)
 admin.site.register(models.Version, VersionAdmin)
