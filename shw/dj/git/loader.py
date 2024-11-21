35b54f2ac359318e62fe25ea6dbba949864f2299
alex <alex@pdp7.net>
2024-11-21 21:01:34 +0100

    Refactor function to load Git versions for a single app

--- shw/dj/git/loader.py
+++ shw/dj/git/loader.py
@@ -6,14 +6,18 @@ def load():
     broken = {}
     for git_app in models.GitApp.objects.all():
         try:
-            tags_to_commits = git.get_remote_tags_to_dates(git_app.remote_url)
-
-            for tag, date in tags_to_commits.items():
-                models.Version.objects.update_or_create(
-                    git_app=git_app,
-                    version=tag,
-                    defaults={"date": date}
-                )
+            load_app(git_app)
         except Exception as e:
             broken[git_app.name] = e
     assert not broken, f"Broken repos {broken}"
+
+
+def load_app(git_app):
+    tags_to_commits = git.get_remote_tags_to_dates(git_app.remote_url)
+
+    for tag, date in tags_to_commits.items():
+        models.Version.objects.update_or_create(
+            git_app=git_app,
+            version=tag,
+            defaults={"date": date}
+        )
