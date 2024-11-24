5c4a395c88d9ab826e6feffac6226cef31f91cad
alex <alex@pdp7.net>
2024-11-24 10:47:07 +0100

    Add latest updates to have some "liveness" perception

--- shw/dj/updates/views.py
+++ shw/dj/updates/views.py
@@ -6,6 +6,11 @@ from shw.dj.updates import models
 class AppListView(ListView):
     template_name = "updates/app_list.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["recent_updates"] = models.Update.objects.all().order_by("-date")[0:10]
+        return context
+
     def get_queryset(self):
         return models.Update.objects.values("name").distinct().order_by("name")
 
