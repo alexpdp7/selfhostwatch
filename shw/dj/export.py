ff3ad750d68230e421a82d6ee7797e27a8942d57
alex <alex@pdp7.net>
2024-12-18 12:49:23 +0100

    Add Django data dump to export

--- shw/dj/export.py
+++ shw/dj/export.py
@@ -3,12 +3,16 @@ import pathlib
 import subprocess
 import tempfile
 
+from django.core.management.commands import dumpdata
+
 
 def export():
     http_host_port = os.environ["EXPORT_HTTP_HOST_PORT"]
     with tempfile.TemporaryDirectory() as tempdir:
         tempdir = pathlib.Path(tempdir)
         mirrordir = tempdir / http_host_port / "updates"
+        mirrordir.mkdir(parents=True, exist_ok=True)
+
         subprocess.run(
             [
                 "wget",
@@ -19,6 +23,13 @@ def export():
             check=True,
             cwd=tempdir,
         )
+
+        dumpdata.Command().handle("git", "yuno",
+                                  format="json",
+                                  output=mirrordir / "dump.json",
+                                  verbosity=0, exclude=[],
+                                  indent=None, database=None, traceback=None, use_natural_foreign_keys=None, use_natural_primary_keys=None, use_base_manager=None, primary_keys=None)
+
         subprocess.run(["git", "init"], check=True, cwd=mirrordir)
         subprocess.run(["git", "add", "."], check=True, cwd=mirrordir)
         subprocess.run(
